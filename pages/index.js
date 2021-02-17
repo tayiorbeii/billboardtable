a28fb1c654b2527f61db6f13c1663940aa607ef7
Taylor Bell <taylorbell@gmail.com>
2021-02-17 13:02:08 -0700

    images flickering

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React from "react";
 import { readRemoteFile } from "react-papaparse";
-import {useRowSelect, useTable} from 'react-table'
+import {useRowSelect, useTable, useBlockLayout} from 'react-table'
+import {FixedSizeList} from 'react-window'
 
 export default function Home() {
   const [data, setData] = React.useState([]);
@@ -26,12 +27,31 @@ export default function Home() {
   const columns = React.useMemo(() => [
     {
       Header: 'Rank',
-      accessor: 'rank'
+      accessor: 'rank',
+      width: 30
     },
     {
+      Header: 'Song Title',
       accessor: 'track'
+    },
+    {
+      Header: 'Artist',
+      accessor: 'primaryArtist'
+    },
+    {
+      Header: 'Artwork',
+      accessor: 'albumArtUrl',
+      Cell: ({value}) => console.log(value ? value : 'http://www.fillmurray.com/100/100') || <img src={value ? value : 'http://www.fillmurray.com/100/100'} />,
+      width: 100
     }
   ], [])
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      width: 300,
+    }),
+    []
+  )
 
 
 
@@ -41,33 +61,40 @@ export default function Home() {
     headerGroups,
     rows,
     prepareRow,
+    totalColumnsWidth
   } = useTable({
     columns,
     data,
-  })
+    defaultColumn
+  }, useBlockLayout)
+
 
   return (
     <table {...getTableProps()}>
       <thead>
-        {headerGroups.map(headerGroup => (
+        {headerGroups.map(headerGroup => {
+          return (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
               <th {...column.getHeaderProps()}>{column.render('Header')}</th>
             ))}
           </tr>
-        ))}
+        )})}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              })}
-            </tr>
-          )
-        })}
+        <FixedSizeList height={800} width={totalColumnsWidth} itemCount={rows.length} itemSize={100}>
+          {({index, style}) => {
+            const row = rows[index]
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          }}
+        </FixedSizeList>
       </tbody>
     </table>
   );
